31ef0c0b5b5a5c2016b9d1303d83bb75ab5abf9f
Pratik Bassi <pratik.bassi@mail.utoronto.ca>
2020-04-08 00:46:57 +0000

    added testing for products

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor can view a product", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see all products" do
+    # ACT
+    visit root_path
+
+    # DEBUG
+    save_screenshot
+
+    # VERIFY
+    expect(page).to have_css 'article.product'
+    page.all('article.product')[0].click
+    expect(page).to have_content("Quantity")
+
+  end
+
+end
